6b4b8a4b2ede411a8944a09c32394d11eefe6753
Patrolin <patrik@balas.cz>
2021-06-14 22:06:04 +0200

    why is epsilon stupid?

--- qLib.py
+++ qLib.py
@@ -2,23 +2,14 @@ import math
 from math import remainder as rem, modf, hypot as L2
 from typing import *
 
+# add, sub, mul, div, pow, mod, rem
+
 # bitwise (|, &, copysign)
 
-# add, sub, mul, div, pow, isqrt, exp, expm1, log, log2, log10, log1p
+def isqrt(x: float) -> float:
+  pass
 
-def mod(a: float, b: float) -> float:
-  return a % b
-
-def rem(a: float, b: float) -> float:
-  return math.remainder(a, b)
-
-# floor, round, ceil, abs
-
-def trunc(x: float) -> float:
-  return math.trunc(x)
-
-def modf(x: float) -> Tuple[float, float]:
-  return math.modf(x)
+# floor, round, ceil, abs, trunc, modf
 
 # frexp, ldexp, isfinite, isinf, isnan
 
@@ -41,32 +32,12 @@ def C(n: int, k: int) -> int:
 
 # def C(n: float, k: float) -> float:
 
-epsilon = 1e-8
+epsilon = 1e-6
 
 def minimize(x0: List[float], f: Callable[[List[float]], float], ro: float = 0.9) -> List[float]:
   # find a local minimum for a function with continuous gradient using fixed AdaDelta
   mean_square_delta = [0.0] * len(x0)
   mean_square_gradient = [0.0] * len(x0)
-  gradient = 1.0
-  n = 0
-  while abs(gradient) > epsilon:
-    n += 1
-    for j in range(len(x0)):
-      x1 = list(x0)
-      x1[j] += epsilon
-      gradient = (f(x1) - f(x0)) / epsilon
-      mean_square_gradient[j] = ro * mean_square_gradient[j] + (1 - ro) * gradient**2 / n**2
-      delta_x = -((mean_square_delta[j] + epsilon) / (mean_square_gradient[j] + epsilon))**.5 * gradient
-      x0[j] += delta_x
-      mean_square_delta[j] = ro * mean_square_delta[j] + (1 - ro) * delta_x**2 / n**2
-      #print(x1, gradient)
-  print(n)
-  return x0
-
-def _minimize(x0: List[float], f: Callable[[List[float]], float], ro: float = 0.95) -> List[float]:
-  # find a local minimum for a function with continuous gradient using a moving average :/
-  mean_gradients = [0.0] * len(x0)
-  gradient = 1.0
   n = 0
   loop = 1
   while loop:
@@ -76,11 +47,12 @@ def _minimize(x0: List[float], f: Callable[[List[float]], float], ro: float = 0.
       x1 = list(x0)
       x1[j] += epsilon
       gradient = (f(x1) - f(x0)) / epsilon
-      mean_gradients[j] = ro * mean_gradients[j] + (1 - ro) * gradient
-      delta_x = -mean_gradients[j]
       loop |= (abs(gradient) > epsilon)
+      mean_square_gradient[j] = ro * mean_square_gradient[j] + (1 - ro) * gradient**2 / n**2
+      delta_x = -((mean_square_delta[j] + epsilon) / (mean_square_gradient[j] + epsilon))**.5 * gradient
       x0[j] += delta_x
-      print(x0, mean_gradients)
+      mean_square_delta[j] = ro * mean_square_delta[j] + (1 - ro) * delta_x**2 / n**2
+      #print(x1, gradient)
   print(n)
   return x0
 
@@ -88,10 +60,24 @@ phi1 = minimize([1.0], lambda x0: (x0[0]**2 - x0[0] - 1)**2)[0]
 phi2 = minimize([1.0], lambda x0: (x0[0]**3 - x0[0] - 1)**2)[0]
 phi3 = minimize([1.0], lambda x0: (x0[0]**4 - x0[0] - 1)**2)[0]
 phi4 = minimize([1.0], lambda x0: (x0[0]**5 - x0[0] - 1)**2)[0]
-print(phi1, phi2, phi3, phi4) # 1.618033983547484 1.3247179522114612 1.2207440795921793 1.1673039732520734
+print(phi1, phi2, phi3, phi4) # 1.6180334617195697 1.32471745536506 1.2207435828264872 1.1673034776901865
 
-e = minimize([1.0], lambda x0: ((x0[0]**(1 + epsilon) - x0[0]) / epsilon - x0[0])**2)[0]
-print(e) # 2.7182818194466964
+# exp, expm1, log1p
+
+def log(x: float) -> float:
+  return (x**epsilon - 1) / epsilon
+
+def log2(x: float) -> float:
+  return log(x) / log(2)
+
+def log10(x: float) -> float:
+  return log(x) / log(10)
+
+#e = minimize([3.0], lambda x0: ((x0[0]**(1 + epsilon) - x0[0]) / epsilon - x0[0])**2)[0]
+e = minimize([3.0], lambda x0: ((log(x0[0]) - 1)**2))[0]
+print(e) # 2.7182824205318648
+
+print(log(2), log(1), log(e), log(10), log(16))
 
 pi = 0
 delta_x = 1
@@ -116,9 +102,9 @@ def evalPolynomial(W: List[float], x: float) -> float:
   return total
 
 L5 = Legendre(5)
-print(minimize([0.0], lambda x0: evalPolynomial(L5, x0[0])**2))
-print(minimize([0.5], lambda x0: evalPolynomial(L5, x0[0])**2))
-print(minimize([0.0, 0.0], lambda x0: (x0[0] + 1)**2 + (x0[1] + 1)**2))
+#print(minimize([0.0], lambda x0: evalPolynomial(L5, x0[0])**2))
+#print(minimize([0.5], lambda x0: evalPolynomial(L5, x0[0])**2))
+#print(minimize([0.0, 0.0], lambda x0: (x0[0] + 1)**2 + (x0[1] + 1)**2))
 #print(minimize([1.0], lambda x0: evalPolynomial(L5, x0[0])**2))
 #print([minimize([i / 10], lambda x0: evalPolynomial(L5, x0[0])**2) for i in range(-10, 11, 1)])
 
