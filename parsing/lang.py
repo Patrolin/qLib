9a5bb2a42cb96be3f512b9bf47021728d14bdaed
Patrolin <patrik@balas.cz>
2021-08-24 12:21:02 +0200

    added comments

---
+++ parsing/lang.py
@@ -0,0 +1,54 @@
+'''
+u32 xorshift(u32 x){
+  x ^= x << 13
+  x ^= x << 17
+  x ^= x << 5
+  return x
+}
+main(){
+  if a == b{
+
+  }
+  if a == b{
+
+  }
+  else{
+
+  }
+  switch sign(a-b)
+    -1{
+
+    }
+    0{
+
+    }
+    1{
+
+    }
+  for(u32 i = 0) i < N{
+
+  }
+  for(u32 x) A{
+
+  }
+  while i < N{
+
+  }
+}
+struct Vec3{ // packed?
+  f32 x
+  f32 y
+  f32 z
+  f32 dot(Vec3 self, Vec3 other){
+    return self.x other.x + self.y other.y + self.z other.z
+  }
+}
+enum Token{
+  Int
+  Float
+}
+union Object{
+  Cube
+  Sphere
+}
+'''
