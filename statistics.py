f54a5b500da1374ab0735745987c3ad7d988014c
Patrolin <patrik@balas.cz>
2021-09-03 19:30:22 +0200

    improved mode()

--- statistics.py
+++ statistics.py
@@ -1,3 +1,4 @@
+from collections import deque as LinkedList
 from typing import *
 
 def mean(X: List[float]) -> float:
@@ -20,26 +21,33 @@ def median(X: List[float]) -> float:
     return (X[i]+X[i+1])/2
 
 def mode(X: List[float]) -> float:
-  # return an estimated in-distribution mode of X in O(n^2)
-  A = list(X)
-  u = mean(A)
-  for n in range(len(X)-1, 1, -1):
-    worstI, worstDistance = 0, 0.0
-    for i, x in enumerate(A):
-      distance = abs(u-x)
-      if distance > worstDistance:
-        worstI, worstDistance = i, distance
-    u -= (A[worstI]-u) / n
-    A.pop(worstI)
+  # return an estimated in-distribution mode of a sorted X in O(n)
+  u = mean(X)
+  A = LinkedList(X)
+  for n in range(len(X)-1, 0, -1):
+    # remove farthest neighbor relative to the mean
+    a, aDistance = A[0], abs(A[0]-u)
+    b, bDistance = A[-1], abs(A[-1]-u)
+    if aDistance >= bDistance:
+      u -= (a-u) / n
+      A.popleft()
+    else:
+      u -= (b-u) / n
+      A.pop()
   return A[0]
-  # is this better? https://www.sciencedirect.com/science/article/abs/pii/S016771520900354X
+  # d > 1
+    # https://stackoverflow.com/questions/59672100/how-to-find-farthest-neighbors-in-euclidean-space
+    # http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.386.8193&rep=rep1&type=pdf
+    # https://en.wikipedia.org/wiki/Priority_queue
+    # https://en.wikipedia.org/wiki/R-tree
+    # https://en.wikipedia.org/wiki/Ball_tree
 
 # quantiles? https://www.cs.wustl.edu/~jain/papers/ftp/psqr.pdf
 
 if __name__ == '__main__':
-  X = [0, .24, .25, 1]
+  X = sorted([0, .24, .25, 1])
   print(mode(X), X)
 
   phi = (1 + 5**.5)/2
-  X = [(0.5 + i*1/phi) % 1 for i in range(5)]
+  X = sorted([(0.5 + i*1/phi) % 1 for i in range(6)])
   print(mode(X), X)
